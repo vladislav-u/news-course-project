f188f5b31eb8430f1ef0b330cf84ace6ea855b1d
Vladislav Ulynets <vladislav.ulynets@outlook.com>
2023-04-17 10:40:44 +0300

    Added connection to MongoDB and changed imports

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 5000;
+
+try {
+	await mongoose.connect(
+		'mongodb+srv://vladislavulynets:courseProjectPassword@courseprojectdb.a7kbrwe.mongodb.net/newsDB'
+	);
+	console.log('Connected to MongoDB');
+} catch (error) {
+	console.log('Failed to connect to MongoDB', error);
+}
+
+app.use(express.json());
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
