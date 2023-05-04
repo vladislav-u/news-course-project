40e007023439393eeb6697c0ab22938f4d473ae7
vladislav-u <vladislav.ulynets@outlook.com>
2023-05-04 23:38:14 +0300

    Added functionality to search bar, made minor design fixes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -18,3 +18,26 @@ export const homeView = (req, res) => {
 
 	getArticles();
 };
+
+export const homeSearch = (req, res) => {
+	const url =
+		'https://gnews.io/api/v4/search?q=' +
+		req.query.searchQuery +
+		'&apikey=' +
+		process.env.GNEWS_KEY;
+
+	let articles = [];
+	async function getArticles() {
+		await fetch(url)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				articles = data.articles;
+			});
+
+		res.render('index', { articles: articles });
+	}
+
+	getArticles();
+};
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import { homeView } from '../controllers/homeController.js';
+import { homeSearch, homeView } from '../controllers/homeController.js';
 
 const homeRouter = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 homeRouter.get('/', homeView);
+homeRouter.get('/search', homeSearch);
 
 export default homeRouter;
