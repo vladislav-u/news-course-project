10f9a6bd11fff9d17de3b0f42b2679198d12bee2
vladislav-u <vladislav.ulynets@outlook.com>
2023-05-18 22:09:06 +0300

    Removed .env usage in files

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { TOKEN_KEY } from '../config.js';
 
 const verifyToken = (req, res, next) => {
 	const token = req.cookies.token;
@@ -9,7 +10,7 @@ const verifyToken = (req, res, next) => {
 		});
 	}
 	try {
-		req.user = jwt.verify(token, process.env.TOKEN_KEY);
+		req.user = jwt.verify(token, TOKEN_KEY);
 	} catch (error) {
 		return res.status(401).send('Invalid token.');
 	}
--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,9 +1,7 @@
-import bodyParser from 'body-parser';
 import express from 'express';
 import { homeSearch, homeView } from '../controllers/homeController.js';
 
 const homeRouter = express.Router();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 homeRouter.get('/', homeView);
 homeRouter.get('/search', homeSearch);
